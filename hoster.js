3063469ea69878a499e0385ffa771068ac61337f
XTawhidX <96291717+XTawhidX@users.noreply.github.com>
2022-06-25 18:33:59 +0600

    Add files via upload

---
+++ hoster.js
@@ -0,0 +1,25 @@
+const api = require("discord.js");
+const fs = require('fs');
+
+const data = fs.readFileSync('tokens.txt', 'UTF-8');
+const lines = data.split(/\r?\n/);
+
+console.log("All Token Ar Online");
+
+lines.forEach((line) => {
+    Hosting(line);
+});
+
+function Hosting(token)
+{
+    const userclient = new api.Client()
+    userclient.on("ready", () => { 
+        console.log('Connected: ' + userclient.user.tag);
+    });
+    userclient.login(token);
+}
+
+process.on('uncaughtException', function (exception) {
+});
+process.on('unhandledRejection', error => {
+});
